39f4135e21f9598a42dc80df1102aae68f1c7b95
Sanyam Kangtani <sanyamkangtani803@gmail.com>
2022-10-15 23:11:39 +0530

    final commit

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -7,10 +7,10 @@ import {Routes,Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-    <Routes>
+     <Routes>
       <Route path = "/" element = {<Home />}></Route>
       <Route path = "/:id" element = {<SingleProducts />}></Route>
-    </Routes>
+     </Routes>
 
     </div>
   );
